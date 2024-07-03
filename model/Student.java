20b44c15e94ede3e0cc64fd1fb0e8358bd6bc304
VL <veranikalis11@gmail.com>
2024-07-03 18:59:49 +0200

    s5

---
+++ model/Student.java
@@ -0,0 +1,10 @@
+package model;
+public class Student extends User{
+
+    int groupId;
+
+    public Student(int id, String name, String lastName, int groupId) {
+        super(id, name, lastName);
+        this.groupId = groupId;
+    }  
+}
---
+++ model/Teacher.java
@@ -0,0 +1,17 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends User{
+
+    private final List<Integer> groups = new ArrayList<>();
+    
+    public Teacher(int id, String name, String lastName) {
+        super(id, name, lastName);
+    }
+
+    public void addGroupId(int id){//обязательно убрать в сервис (ошибка проектирования)
+        groups.add(id);
+    }
+    
+}
